8daba44e7b73efc6711092b6323d51ace6511992
Sim1Desjardins <simonedesjardins7@gmail.com>
2025-02-24 11:25:26 -0500

    Test cases

--- src/main.py
+++ src/main.py
@@ -1,14 +1,9 @@
 from textnode import TextNode, TextType
 
-def main():
-    # Create a TextNode instance here with some test values
-    test_node = TextNode(
-        "This is a text node",
-        TextType.BOLD_TEXT,
-        "https://www.boot.dev"
-    )
-    # Print the instance
-    print(test_node)
 
-if __name__ == "__main__":
-    main()
+def main():
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
+
+
+main()
