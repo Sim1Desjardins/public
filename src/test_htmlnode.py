a477049c850e0079336640e0ba50ed46eccf82dc
Sim1Desjardins <simonedesjardins7@gmail.com>
2025-02-27 10:40:33 -0500

    ParentNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -48,6 +48,45 @@ class TestHTMLNode(unittest.TestCase):
             node.__repr__(),
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    leaf1 = LeafNode("span", "child1")
+    leaf2 = LeafNode("strong", "child2")
+    parent = ParentNode("div", [leaf1, leaf2])
+    print(parent.to_html())  # Should output: <div><span>child1</span><strong>child2</strong></div>
+
+    grandchild = LeafNode("i", "grandchild")
+    child = ParentNode("p", [grandchild])
+    parent = ParentNode("div", [child])
+    print(parent.to_html())  # Should output: <div><p><i>grandchild</i></p></div>
 
 
 if __name__ == "__main__":
