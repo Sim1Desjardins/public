351b828c238c13b2e04b0e2c060787ca11a49dfb
Sim1Desjardins <simonedesjardins7@gmail.com>
2025-05-12 13:40:47 -0400

    Configure for GitHub Pages deployment

--- src/main.py
+++ src/main.py
@@ -6,17 +6,17 @@ from gencontent import generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs "
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
-    print("Deleting public directory...")
+    print("Deleting docs directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-    print("Copying static files to public directory...")
+    print("Copying static files to docs directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
